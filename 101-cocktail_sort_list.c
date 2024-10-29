deb013c010d110fc4f6068b69922695f379ffde9
FRANKD-UX <frankdafrica@gmail.com>
2024-10-29 06:48:49 -0700

    f

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,6 +1,35 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - Swaps two adjacent nodes in a doubly linked list.
+ * @left: Pointer to the left node.
+ * @right: Pointer to the right node.
+ */
+void swap_nodes(listint_t *left, listint_t *right)
+{
+	listint_t *temp = right->next;
+
+	if (left->prev)
+		left->prev->next = right;
+	if (right->next)
+		right->next->prev = left;
+
+	right->prev = left->prev;
+	left->next = temp;
+	right->next = left;
+	left->prev = right;
+
+	if (temp)
+		temp->prev = left;
+
+	/* Update the list head if necessary */
+	if (right->prev == NULL)
+	{
+		*list = right; /* Update head if necessary */
+	}
+}
+
+/**
  * cocktail_sort_list - Sorts a doubly linked list using the cocktail sort
  * algorithm.
  * @list: Pointer to the list to sort.
@@ -17,6 +46,8 @@ void cocktail_sort_list(listint_t **list)
 	while (swapped)
 	{
 		swapped = 0;
+
+		/* Forward pass */
 		while (head->next)
 		{
 			if (head->n > head->next->n)
@@ -24,11 +55,15 @@ void cocktail_sort_list(listint_t **list)
 				swap_nodes(head, head->next);
 				swapped = 1;
 			}
-			head = head->next;
+			else
+				head = head->next;
 		}
+
 		if (!swapped)
 			break;
+
 		swapped = 0;
+		/* Backward pass */
 		while (head->prev)
 		{
 			if (head->n < head->prev->n)
@@ -36,7 +71,8 @@ void cocktail_sort_list(listint_t **list)
 				swap_nodes(head->prev, head);
 				swapped = 1;
 			}
-			head = head->prev;
+			else
+				head = head->prev;
 		}
 	}
 }
