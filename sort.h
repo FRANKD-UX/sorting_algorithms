0f48085a2df1af5091890ce64eaf26e4be6e049c
FRANKD-UX <frankdafrica@gmail.com>
2024-10-29 07:44:23 -0700

    F

--- sort.h
+++ sort.h
@@ -1,28 +1,21 @@
-#ifndef _SORT_H_
-#define _SORT_H_
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef SORT_H
+#define SORT_H
 
-/**
- * struct listint_s - Doubly linked list node
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+#include <stddef.h>
 
-typedef struct listint_s
+/* Doubly linked list data structure */
+typedef struct listint_s 
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
-/** prototypes */
+/* Prototype for print functions */
+void print_array(const int *array, size_t size); /* array printing */
+void print_list(const listint_t *list);          /* linked list printing */
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
-/** Task functions */
+/* Task function prototypes */
 int len_list(listint_t *h);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
@@ -36,8 +29,7 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void merge_recursion(int *arr, int *array, size_t left, size_t right);
-void merge_subarray(int *arr, int *array, size_t left,
-		size_t middle, size_t right);
+void merge_subarray(int *arr, int *array, size_t left, size_t middle, size_t right);
 void heap_sort(int *array, size_t size);
 void heapify(int *array, size_t s, size_t root, size_t size);
 void radix_sort(int *array, size_t size);
@@ -45,6 +37,5 @@ void count_sort_LSD(int *array, size_t size, size_t lsd);
 void quick_sort_hoare(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void swap_nodes(listint_t *left, listint_t *right);
-void print_array(int *array, size_t size);
 
-#endif /** _SORT_H_ */
+#endif /* SORT_H */
