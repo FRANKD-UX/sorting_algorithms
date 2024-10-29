aa6b76a802eea36a701e2d5a50ce4a7167fcdb53
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-10-29 13:01:46 -0700

    Heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,66 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * swap - Swaps two integers in an array.
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer.
- */
-void swap(int *a, int *b)
+* stupify - recurrssive heapfiy function
+* @array: Array to sort
+* @heap: size of heap data
+* @i: index
+* @size: size of array
+*/
+
+void stupify(int *array, int heap, int i, int size)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
+	int large = i, l = 2 * i + 1, r = 2 * i + 2, t;
 
-/**
- * heapify - Maintains the max-heap property for a subtree.
- * @array: The array representing the heap.
- * @size: The size of the heap.
- * @i: The index of the root of the subtree.
- */
-void heapify(int *array, size_t size, size_t i)
-{
-	size_t largest = i;
-	size_t left = 2 * i + 1;
-	size_t right = 2 * i + 2;
-
-	if (left < size && array[left] > array[largest])
-		largest = left;
-
-	if (right < size && array[right] > array[largest])
-		largest = right;
-
-	if (largest != i)
+	if (l < heap && array[l] > array[large])
+		large = l;
+	if (r < heap && array[r] > array[large])
+		large = r;
+	if (large != i)
 	{
-		swap(&array[i], &array[largest]);
+		t = array[i], array[i] = array[large], array[large] = t;
 		print_array(array, size);
-		heapify(array, size, largest);
+		stupify(array, heap, large, size);
 	}
 }
 
 /**
- * heap_sort - Sorts an array of integers in ascending order
- *             using the Heap Sort algorithm.
- * @array: The array to be sorted.
- * @size: The size of the array.
- */
+* heap_sort - Sorts array with heap sort algo
+* @array: array to sort
+* @size: Size of array to sort
+*/
+
 void heap_sort(int *array, size_t size)
 {
-	int i;
+	int i = size / 2 - 1, temp;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, size, i);
-
-	for (i = size - 1; i > 0; i--)
+	for (; i >= 0; i--)
+		stupify(array, size, i, size);
+	for (i = size - 1; i >= 0; i--)
 	{
-		swap(&array[0], &array[i]);
-		print_array(array, size);
-		heapify(array, i, 0);
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		if (i > 0)
+			print_array(array, size);
+		stupify(array, i, 0, size);
 	}
-}
 
+}
