d75b91438665fdaec6dc64a407134f20da7f1ee2
FRANKD-UX <frankdafrica@gmail.com>
2024-10-29 06:46:39 -0700

    f

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,93 +1,43 @@
 #include "sort.h"
 
 /**
- * list_len - function returns length of list
- * @list: head of list
- *
- * Return: length
+ * cocktail_sort_list - Sorts a doubly linked list using the cocktail sort
+ * algorithm.
+ * @list: Pointer to the list to sort.
  */
-
-size_t list_len(listint_t *list)
-{
-	size_t length = 0;
-
-	while (list)
-	{
-		length++;
-		list = list->next;
-	}
-	return (length);
-}
-
-/**
- * switch_nodes - function will swap nodes at pointer p with the following node
- * @list: head of list
- * @p: pointer to node
- */
-
-void switch_nodes(listint_t **list, listint_t **p)
-{
-	listint_t *a, *b, *c, *d;
-
-	a = (*p)->prev;
-	b = *p;
-	c = (*p)->next;
-	d = (*p)->next->next;
-	two->next = d;
-	if (d)
-		d->prev = b;
-	c->next = b;
-	c->prev = b->prev;
-	if (a)
-		a->next = c;
-	else
-		*list = c;
-	b->prev = c;
-	*p = c;
-}
-
-/**
- * cocktail_sort_list - function sorts a doubly linked list using
- * the cocktail sort algorithm
- * @list: pointer to list
- */
-
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *p;
-	int s = 0;
+	listint_t *head;
+	int swapped = 1;
 
-	if (!list || !*list || list_len(*list) < 2)
+	if (list == NULL || *list == NULL)
 		return;
-	p = *list;
-	while (!s)
+
+	head = *list;
+	while (swapped)
 	{
-		s = 1;
-		while (p->next)
+		swapped = 0;
+		while (head->next)
 		{
-			if (p->n > p->next->n)
+			if (head->n > head->next->n)
 			{
-				s = 0;
-				switch_nodes(list, &p);
-				print_list(*list);
+				swap_nodes(head, head->next);
+				swapped = 1;
 			}
-			else
-				p = p->next;
+			head = head->next;
 		}
-		if (s)
+		if (!swapped)
 			break;
-		p = p->prev;
-		while (p->prev)
+		swapped = 0;
+		while (head->prev)
 		{
-			if (p->n < p->prev->n)
+			if (head->n < head->prev->n)
 			{
-				s = 0;
-				p = p->prev;
-				switch_nodes(list, &p);
-				print_list(*list);
+				swap_nodes(head->prev, head);
+				swapped = 1;
 			}
-			else
-				p = p->prev;
+			head = head->prev;
 		}
 	}
 }
+
